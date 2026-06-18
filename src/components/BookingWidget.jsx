03bb5ca628b5b59944af41826ba7bebe6c95efa5
Shohan111369 <sarkershohan346@gmail.com>
2026-06-18 20:57:37 +0800

    now added stripe connect and upgrdaere components

--- src/components/BookingWidget.jsx
+++ src/components/BookingWidget.jsx
@@ -5,51 +5,50 @@ import { Card, Button, Input } from "@heroui/react";
 import { FaCheck } from "react-icons/fa";
 import { useSession } from "@/lib/auth-client";
 
-export default function BookingWidget({ ticketPrice, availableSeats, eventId, eventTitle }) {
-  const [quantity, setQuantity] = useState(0)
+export default function BookingWidget({
+  ticketPrice,
+  availableSeats,
+  eventId,
+  eventTitle,
+}) {
+  const [quantity, setQuantity] = useState(0);
 
   const { data: session } = useSession();
 
   const user = session?.user;
   // console.log(user);
 
-
   const isSoldOut = availableSeats <= 0;
 
   const totalAmount = ticketPrice.toFixed(2) * quantity;
 
   const handleBookTicket = async () => {
-
     const paymentData = {
       type: "booking",
       ticketPrice: ticketPrice.toFixed(2),
       eventId,
       eventTitle,
       quantity,
-    }
+    };
 
     const res = await fetch("/api/checkout_sessions", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(paymentData)
+      body: JSON.stringify(paymentData),
     });
     const data = await res.json();
     // console.log(data);
     if (data?.url) {
       window.location.href = data.url;
     }
-
-
-
-  }
-
+  };
 
   return (
     <Card className="glass border-white/5 sticky top-24" radius="lg">
-      {
-        user?.role == "attendee" ? <div className="p-8 space-y-6">
+      {user?.role == "attendee" ? (
+        <div className="p-8 space-y-6">
           <h3 className="text-xl font-bold text-white">Booking Details</h3>
 
           {/* Stat list */}
@@ -88,9 +87,7 @@ export default function BookingWidget({ ticketPrice, availableSeats, eventId, ev
 
               <div className="flex justify-between items-center text-sm font-semibold text-white pt-2">
                 <span>Total Amount:</span>
-                <span className="text-white text-lg">
-                  ${totalAmount}
-                </span>
+                <span className="text-white text-lg">${totalAmount}</span>
               </div>
             </>
           )}
@@ -98,10 +95,11 @@ export default function BookingWidget({ ticketPrice, availableSeats, eventId, ev
           <Button
             isDisabled={isSoldOut}
             onClick={handleBookTicket}
-            className={`w-full font-bold h-12 shadow-lg ${isSoldOut
-              ? "bg-slate-800 text-slate-500 shadow-none cursor-not-allowed"
-              : "bg-gradient-to-r from-pink-500 to-indigo-600 text-white shadow-pink-500/10 hover:shadow-pink-500/20"
-              }`}
+            className={`w-full font-bold h-12 shadow-lg ${
+              isSoldOut
+                ? "bg-slate-800 text-slate-500 shadow-none cursor-not-allowed"
+                : "bg-gradient-to-r from-pink-500 to-indigo-600 text-white shadow-pink-500/10 hover:shadow-pink-500/20"
+            }`}
             radius="lg"
           >
             {isSoldOut ? "Sold Out" : "Book Ticket Now"}
@@ -111,13 +109,14 @@ export default function BookingWidget({ ticketPrice, availableSeats, eventId, ev
             <FaCheck className="text-green-500 shrink-0" />
             <span>Instant confirmation | Vetted organizers</span>
           </div>
-        </div> : <Card>
+        </div>
+      ) : (
+        <Card>
           <p className="text-red-500">
             {user?.role.toUpperCase()} can not book a event
           </p>
-
         </Card>
-      }
+      )}
     </Card>
   );
 }
